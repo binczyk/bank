fb6c5619f6438b338c05194e576275320b23d7bb
Daniel Biniek <daniel.biniek@comarch.pl>
2018-05-27 14:45:12 +0200

    Dodanie „tytułu przelewu”, ustalanego na page1, widocznego na page2 oraz w asynchronicznych powiadomieniach o nowym przelewie.

--- js/page1.js
+++ js/page1.js
@@ -1,29 +1,35 @@
-app.controller("Page1", [ "$http", "globals",
-	function($http, globals) {
-		var ctrl = this;
-		ctrl.balance = 0;
+app.controller("Page1", ["$http", "globals",
+    function ($http, globals) {
+        var ctrl = this;
+        ctrl.balance = 0;
         ctrl.limit = 0;
         ctrl.recipient = '';
-		ctrl.amount = 0;
+        ctrl.amount = 0;
+        ctrl.title = '';
 
-		$http.get("/account").then(function(rep) {
-			ctrl.balance = rep.data.balance;
-			ctrl.limit = rep.data.limit;
-		});
+        $http.get("/account").then(function (rep) {
+            ctrl.balance = rep.data.balance;
+            ctrl.limit = rep.data.limit;
+        });
 
-		ctrl.transfer = function() {
-			$http.post('/account', {recipient: ctrl.recipient, amount: ctrl.amount}).then(function (rep) {
-                	ctrl.balance = rep.data.balance;
-                	ctrl.recipient = '';
-                	ctrl.amount = 0;
+        ctrl.transfer = function () {
+            $http.post('/account', {
+                recipient: ctrl.recipient,
+                amount: ctrl.amount,
+                title: ctrl.title
+            }).then(function (rep) {
+                    ctrl.balance = rep.data.balance;
+                    ctrl.recipient = '';
+                    ctrl.amount = 0;
+                    ctrl.title = '';
                     globals.alert.type = 'success';
                     globals.alert.message = 'Transfered successfully';
-            	},
-				function (err) {
-					globals.alert.type = 'danger';
-					globals.alert.message = 'Transfer failed: ' + err.data.err;
+                },
+                function (err) {
+                    globals.alert.type = 'danger';
+                    globals.alert.message = 'Transfer failed: ' + err.data.err;
                 }
-			);
-		}
-	}
+            );
+        }
+    }
 ]);
