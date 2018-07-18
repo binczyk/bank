2398a415250425fc256166438d442d892d676e33
dbiniek <danbiiniek@gmail.com>
2018-07-18 09:54:28 +0200

    Revert "code rearrange"
    This reverts commit 2cc7f62

--- js/page1.js
+++ js/page1.js
@@ -1,35 +1,35 @@
 app.controller("Page1", ["$http", "globals",
-                         function ($http, globals) {
-                             var ctrl = this;
-                             ctrl.balance = 0;
-                             ctrl.limit = 0;
-                             ctrl.recipient = '';
-                             ctrl.amount = 0;
-                             ctrl.title = '';
+    function ($http, globals) {
+        var ctrl = this;
+        ctrl.balance = 0;
+        ctrl.limit = 0;
+        ctrl.recipient = '';
+        ctrl.amount = 0;
+        ctrl.title = '';
 
-                             $http.get("/account").then(function (rep) {
-                                 ctrl.balance = rep.data.balance;
-                                 ctrl.limit = rep.data.limit;
-                             });
+        $http.get("/account").then(function (rep) {
+            ctrl.balance = rep.data.balance;
+            ctrl.limit = rep.data.limit;
+        });
 
-                             ctrl.transfer = function () {
-                                 $http.post('/account', {
-                                     recipient: ctrl.recipient,
-                                     amount: ctrl.amount,
-                                     title: ctrl.title
-                                 }).then(function (rep) {
-                                             globals.alert.message = ctrl.title + ' transferred successfully';
-                                             ctrl.balance = rep.data.balance;
-                                             ctrl.recipient = '';
-                                             ctrl.amount = 0;
-                                             ctrl.title = '';
-                                             globals.alert.type = 'success';
-                                         },
-                                         function (err) {
-                                             globals.alert.type = 'danger';
-                                             globals.alert.message = ctrl.title + ' transfer failed: ' + err.data.err;
-                                         }
-                                 );
-                             }
-                         }
+        ctrl.transfer = function () {
+            $http.post('/account', {
+                recipient: ctrl.recipient,
+                amount: ctrl.amount,
+                title: ctrl.title
+            }).then(function (rep) {
+                    ctrl.balance = rep.data.balance;
+                    ctrl.recipient = '';
+                    ctrl.amount = 0;
+                    ctrl.title = '';
+                    globals.alert.type = 'success';
+                    globals.alert.message = 'Transfered successfully';
+                },
+                function (err) {
+                    globals.alert.type = 'danger';
+                    globals.alert.message = 'Transfer failed: ' + err.data.err;
+                }
+            );
+        }
+    }
 ]);
--- js/page3.js
+++ js/page3.js
@@ -1,4 +1,4 @@
-app.controller("Page3", ["$http", "common", function ($http, common) {
+app.controller("Page3", [ "$http", "common", function($http, common) {
 
     var ctrl = this;
 
@@ -7,47 +7,42 @@ app.controller("Page3", ["$http", "common", function ($http, common) {
         chart: {
             type: 'lineChart',
             height: 450,
-            margin: {top: 20, right: 90, bottom: 75, left: 50},
-            showValues: false,
-            x: function (d) {
-                return new Date(d.date);
-            },
-            y: function (d) {
-                return d.after;
-            },
+            margin: { top: 20, right: 90, bottom: 75, left: 50 },
+			showValues: false,
+            x: function(d) { return new Date(d.date); },
+            y: function(d) { return d.after; },
             duration: 500,
             xAxis: {
-                axisLabel: 'Time',
-                rotateLabels: 30,
-                tickFormat: function (d) {
-                    return common.dateFormat(d);
-                },
-                showMaxMin: false
-            },
+            	axisLabel: 'Time',
+				rotateLabels: 30,
+				tickFormat: function(d) { return common.dateFormat(d); },
+				showMaxMin: false
+			},
             yAxis: {
-                axisLabel: 'Balance',
-                axisLabelDistance: -10,
-                showMaxMin: false
-            },
+            	axisLabel: 'Balance',
+				axisLabelDistance: -10,
+				showMaxMin: false
+			},
             xScale: d3.time.scale(),
-            interpolate: 'step-before',
-            clipEdge: false
+			interpolate: 'step-before',
+			clipEdge: false
         }
 
     };
 
     ctrl.data = [
         {
-            key: 'Account balance',
-            values: []
+        	key: 'Account balance',
+			values: []
         }
-    ];
+	];
 
-    $http.get('/history/').then(function (rep) {
+    $http.get('/history/').then(function(rep) {
         ctrl.data[0].values = rep.data;
-        $http.get('/account').then(function (rep) {
-            ctrl.data[0].values.unshift({date: new Date().toString(), after: rep.data.balance});
+        $http.get('/account').then(function(rep) {
+            ctrl.data[0].values.unshift({ date: new Date().toString(), after: rep.data.balance });
         });
     });
+
 
 }]);
